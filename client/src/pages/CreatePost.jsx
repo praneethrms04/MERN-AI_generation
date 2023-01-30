5ab3d673dad213e90cc4c8c82afa24311123ffbe
praneethrms04 <praneethrms04@gmail.com>
2023-01-30 17:43:45 +0530

    final commit

--- client/src/pages/CreatePost.jsx
+++ client/src/pages/CreatePost.jsx
@@ -20,7 +20,7 @@ const CreatePost = () => {
     if (form.prompt) {
       try {
         setGeneratingImg(true);
-        const response = await fetch("http://localhost:5000/api/v1/dalle", {
+        const response = await fetch("https://dall-e-pf9k.onrender.com/api/v1/dalle", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -51,7 +51,7 @@ const CreatePost = () => {
     if (form.prompt && form.photo) {
       setIsLoading(true);
       try {
-        const response = await fetch("http://localhost:5000/api/v1/post", {
+        const response = await fetch("https://dall-e-pf9k.onrender.com/api/v1/post", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -20,7 +20,7 @@ const Home = () => {
   const fetchPosts = async () => {
     setIsLoading(true);
     try {
-      const response = await fetch("http://localhost:5000/api/v1/post", {
+      const response = await fetch("https://dall-e-pf9k.onrender.com/api/v1/post", {
         method: "GET",
         headers: {
           "Content-type": "application/json",
